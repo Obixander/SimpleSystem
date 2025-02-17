1f5dbf5757e7c1b97f689c530b924398b302cd60
obixander <noahhauge@gmail.com>
2025-02-17 14:43:40 +0100

    should be fixed...

--- SimpleApi/Controllers/MockController.cs
+++ SimpleApi/Controllers/MockController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 
 namespace SimpleApi.Controllers
 {
@@ -8,29 +7,9 @@ namespace SimpleApi.Controllers
     [ApiController]
     public class MockController : ControllerBase
     {
-        private string connectionString = "Data Source=localhost;Initial Catalog=SimpleDatabase;User ID=sa;Password=********;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
         [HttpGet]
         public IActionResult Get()
         {
-            //string query = "SELECT TOP 1 * FROM SimpleTable ORDER BY id"; // Modify table and column names
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    SqlCommand command = new SqlCommand(query, connection);
-            //    connection.Open();
-
-            //    using (SqlDataReader reader = command.ExecuteReader())
-            //    {
-            //        if (reader.Read()) // Reads the first row
-            //        {
-            //            return Ok(reader["Column2"]);
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("No data found.");
-            //        }
-            //    }
-            //}
-
             return Ok("Hello World");
         }
 
